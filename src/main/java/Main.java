7bf997497e20ebca2b5de65cd76255ad2b441696
Ankit Raj <araj2805@gmail.com>
2024-10-19 21:14:44 +0530

    Added decoded support for list

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import com.dampcake.bencode.Type;
 import com.google.gson.Gson;
-// import com.dampcake.bencode.Bencode; - available if you need it!
+
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
+import com.dampcake.bencode.Bencode;
+ //- available if you need it!
 
 public class Main {
   private static final Gson gson = new Gson();
@@ -28,7 +33,10 @@ public class Main {
   }
 
   static Object decodeBencode(String bencodedString) {
-    if (Character.isDigit(bencodedString.charAt(0))) {
+    Bencode bencode = new Bencode();
+    char firstChar = bencodedString.charAt(0);
+    Object decoded;
+    if (Character.isDigit(firstChar)) {
       int firstColonIndex = 0;
       for(int i = 0; i < bencodedString.length(); i++) { 
         if(bencodedString.charAt(i) == ':') {
@@ -37,9 +45,66 @@ public class Main {
         }
       }
       int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
+      decoded = bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
+    } else if (firstChar == 'i') {
+      // bencoded number
+      decoded = bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8), Type.NUMBER);
+    } else if (firstChar == 'l') {
+      // bencoded list
+      decoded = bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8), Type.LIST);
+    }
+    else {
+      throw new RuntimeException("Only strings are supported at the moment");
+    }
+    return gson.toJson(decoded);
+  }
+
+  Object decodeBencoded(String bencodedString) {
+    if (Character.isDigit(bencodedString.charAt(0))) {
+      int firstColonIndex = 0;
+      for(int i = 0; i < bencodedString.length(); i++) {
+        if(bencodedString.charAt(i) == ':') {
+          firstColonIndex = i;
+          break;
+        }
+      }
+      int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
       return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
     } else if (bencodedString.charAt(0) == 'i' && bencodedString.charAt(bencodedString.length() - 1) == 'e') {
-      return Long.parseLong(bencodedString.substring(1,bencodedString.length() - 1));
+      return (Object) Long.parseLong(bencodedString.substring(1,bencodedString.length() - 1));
+    } else if (bencodedString.charAt(0) == 'l' && bencodedString.charAt(bencodedString.length() - 1) == 'e') {
+
+      /*int length = 0;
+      if (Character.isDigit(bencodedString.charAt(1)))
+        length = bencodedString.charAt(1);*/
+
+      String[] list = bencodedString.substring(3).split("i");
+
+      StringBuilder lastString = new StringBuilder(String.valueOf(list[list.length - 1]));
+      for (int i = lastString.length() - 1; i >= 0; i--) {
+        if (lastString.charAt(i) == 'e')
+          lastString = lastString.deleteCharAt(i);
+      }
+      list[list.length - 1] = String.valueOf(lastString);
+
+      Pattern numberPattern = Pattern.compile("\\d+");
+      Pattern decimalPattern = Pattern.compile("(\\.\\d+)?");
+      Pattern negativeNumberPattern = Pattern.compile("-?\\d+");
+      Pattern negativeDecimalPattern = Pattern.compile("-?(\\.\\d+)?");
+      Pattern stringPatter = Pattern.compile("[A-Za-z]]");
+
+      for (int i = 0; i < list.length; i++) {
+        String str = (String)list[i];
+        if (numberPattern.matcher(str).matches() || negativeNumberPattern.matcher(str).matches()) {
+          list[i] = String.valueOf(Integer.parseInt(str));
+        } else if (decimalPattern.matcher(str).matches() || negativeDecimalPattern.matcher(str).matches()) {
+          list[i] = String.valueOf(Double.parseDouble(str));
+        } else if (stringPatter.matcher(str).matches()) {
+          list[i] = String.valueOf(str);
+        }
+      }
+
+      return list;
     }
     else {
       throw new RuntimeException("Only strings are supported at the moment");
